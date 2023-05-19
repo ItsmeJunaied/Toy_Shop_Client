592abd3087163cf260bf5b3a7bda0c84b6a54b94
Junaied <junaied526@gmail.com>
2023-05-20 04:33:46 +0600

    10th commit allToy table

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -3,7 +3,53 @@ import './AllToys.css';
 const AllToys = () => {
     return (
         <div>
-            
+            <div className="table">
+
+                <div className="table__header">
+                    <h1>Customer's Orders</h1>
+                    <div className="input-group border">
+                        <input type="search" placeholder="                         Search Data..." />
+                    </div>
+                    <div className="export__file">
+                        <label for="export-file" className="export__file-btn" title="Export File"></label>
+                        <input type="checkbox" id="export-file" />
+                    </div>
+                </div>
+                <div className="table__body">
+                    <table>
+                        <thead>
+                            <tr>
+                                {/* Seller: (if available) show the name of the person who posted the toy
+                                Toy Name
+                                Sub-category
+                                Price
+                                Available Quantity
+                                View Details button */}
+                                <th> Seller</th>
+                                <th> Toy Name</th>
+                                <th> Sub-category </th>
+                                <th> Price</th>
+                                <th> Available Quantity </th>
+                                <th> View Details </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> 1 </td>
+                                <td> </td>
+                                <td>
+                                    <p className="status delivered">Delivered</p>
+                                </td>
+                                <td> 17 Dec, 2022 </td>
+                                <td>
+                                    Delivered
+                                </td>
+                                <td> <button className=" btn btn-accent">Details</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
